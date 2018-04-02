c569f1e09988a7e9c4908bbac3129ed320208a7c
Matt Conrad <mattconrad@gmail.com>
2018-04-02 09:16:12 -0500

    Still fiddling with base maps.

--- Tilesets/background.tsx
+++ Tilesets/background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="background" tilewidth="32" tileheight="32" tilecount="4800" columns="75">
- <image source="../Assembled/background.png" width="2400" height="2048"/>
+<tileset name="background" tilewidth="32" tileheight="32" tilecount="3723" columns="73">
+ <image source="../Assembled/background.png" width="2336" height="1632"/>
 </tileset>
--- Tilesets/middleground.tsx
+++ Tilesets/middleground.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="middleground" tilewidth="32" tileheight="32" tilecount="4140" columns="115">
- <image source="../Assembled/middleground.png" width="3680" height="1152"/>
+<tileset name="middleground" tilewidth="32" tileheight="32" tilecount="3432" columns="156">
+ <image source="../Assembled/middleground.png" width="4992" height="704"/>
 </tileset>
