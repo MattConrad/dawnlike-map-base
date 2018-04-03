0744bc9fdc93fbb9d616d72656ebb8055cfe8911
Matt Conrad <mattconrad@gmail.com>
2018-04-02 19:23:05 -0500

    Start adding properties to tiles.

--- Tilesets/background.tsx
+++ Tilesets/background.tsx
@@ -1,4 +1,484 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="background" tilewidth="32" tileheight="32" tilecount="3723" columns="73">
  <image source="../Assembled/background.png" width="2336" height="1632"/>
+ <tile id="0">
+  <properties>
+   <property name="set-marker" value="upperleft-a-walls"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="set-marker" value="upperleft-b-walls"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="153">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="156">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="158">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="159">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="219">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="221">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="223">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="227">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="229">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="231">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="235">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="236">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="237">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="238">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="239">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="240">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="308">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="309">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="310">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="311">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="312">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="313">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="381">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="382">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="383">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="384">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="385">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="386">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="454">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="455">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="456">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="457">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="458">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="459">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="527">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="528">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="529">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="530">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="531">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="532">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="600">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="601">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="602">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="603">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="604">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="605">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="673">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="674">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="675">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="676">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="677">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="678">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="746">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="747">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="748">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="749">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="750">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="751">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="819">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="820">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="821">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="822">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="823">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="824">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="892">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="893">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="894">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="895">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="896">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="897">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="965">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="966">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="967">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="968">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="969">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="970">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="1038">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="1039">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="1040">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="1041">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="1042">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="1043">
+  <properties>
+   <property name="obstacle" value="stone"/>
+  </properties>
+ </tile>
+ <tile id="2270">
+  <properties>
+   <property name="set-marker" value="lowerright-a-walls"/>
+  </properties>
+ </tile>
+ <tile id="2278">
+  <properties>
+   <property name="set-marker" value="lowerright-b-walls"/>
+  </properties>
+ </tile>
 </tileset>
--- Tilesets/middleground.tsx
+++ Tilesets/middleground.tsx
@@ -1,4 +1,104 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="middleground" tilewidth="32" tileheight="32" tilecount="3432" columns="156">
  <image source="../Assembled/middleground.png" width="4992" height="704"/>
+ <tile id="16">
+  <properties>
+   <property name="set-marker" value="upperleft-a-doors"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="upperright-a-doors" value=""/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="set-marker" value="upperleft-b-doors"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="set-marker" value="upperright-b-doors"/>
+  </properties>
+ </tile>
+ <tile id="2200">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2202">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2204">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2356">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2358">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2360">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2512">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2514">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2516">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2668">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2670">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2672">
+  <properties>
+   <property name="feature" value="door"/>
+  </properties>
+ </tile>
+ <tile id="2980">
+  <properties>
+   <property name="set-marker" value="lowerleft-a-doors"/>
+  </properties>
+ </tile>
+ <tile id="2988">
+  <properties>
+   <property name="set-marker" value="lowerright-a-doors"/>
+  </properties>
+ </tile>
+ <tile id="2989">
+  <properties>
+   <property name="set-marker" value="lowerleft-b-doors"/>
+  </properties>
+ </tile>
+ <tile id="2997">
+  <properties>
+   <property name="set-marker" value="lowerright-b-doors"/>
+  </properties>
+ </tile>
 </tileset>
